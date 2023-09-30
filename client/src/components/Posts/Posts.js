12d2463219f807c5103e9cf61c81c01085643eea
isaiahaquino <isaiahaquino1997@gmail.com>
2023-09-29 18:19:38 -0700

    forms & posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,16 +5,18 @@ import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles"
 
-export default function Posts() {
+export default function Posts({ setCurrentId }) {
   const posts = useSelector((state) => state.posts)
   const classes = useStyles()
+
+  console.log(posts)
   
   return (
-    !posts.lenght ? <CircularProgress /> : (
+    !posts.lenght === 0 ? <CircularProgress /> : (
       <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6}>
-            <Post post={post} />
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
           </Grid>
         ))}
       </Grid>
